62fe10de6e3d37d921a747aad43a5ea1c9d50cad
jlozovei <jlozovei@gmail.com>
2020-05-02 15:21:45 -0300

    feat(tests): add currency test cases + update other tests

--- tests/values.test.js
+++ tests/values.test.js
@@ -1,26 +1,26 @@
-import numbersToWords from '../index';
+import fullNumbers from '../index';
 
 test('Write a single number', () => {
-  const words = numbersToWords(5);
+  const words = fullNumbers(5);
   expect(words).toBe('five');
 });
 
 test('Write a dozen below 20', () => {
-  const words = numbersToWords(14);
+  const words = fullNumbers(14);
   expect(words).toBe('fourteen');
 });
 
 test('Write a dozen above 20', () => {
-  const words = numbersToWords(52);
+  const words = fullNumbers(52);
   expect(words).toBe('fifty-two');
 });
 
 test('Write a hundred', () => {
-  const words = numbersToWords(320);
+  const words = fullNumbers(320);
   expect(words).toBe('three hundred and twenty');
 });
 
 test('Write a million', () => {
-  const words = numbersToWords(4520350);
+  const words = fullNumbers(4520350);
   expect(words).toBe('four million, five hundred and twenty thousand, three hundred and fifty');
 });
